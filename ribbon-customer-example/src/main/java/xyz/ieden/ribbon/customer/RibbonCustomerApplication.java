586e6421338cd3196cfd7c4dcbb513cb20f4d913
gavinlen <gavin_len@163.com>
2018-09-27 19:34:28 +0800

    20180927

--- ribbon-customer-example/src/main/java/xyz/ieden/ribbon/customer/RibbonCustomerApplication.java
+++ ribbon-customer-example/src/main/java/xyz/ieden/ribbon/customer/RibbonCustomerApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 /**
  * @author lianghongwei01
@@ -12,6 +13,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableDiscoveryClient
+@EnableHystrix
 public class RibbonCustomerApplication {
     public static void main(String[] args) {
         SpringApplication.run(RibbonCustomerApplication.class, args);
